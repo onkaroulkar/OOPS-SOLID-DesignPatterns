b97858d80a30a03293c455a78cdb82c8c239a2e3
onkar oulkar <onkaroulkar@gmail.com>
2025-02-13 14:55:03 +0530

    new code

--- src/main/java/org/onkar/DesignPattern/MediatorPatternSolution/WithMediatorPattern.java
+++ src/main/java/org/onkar/DesignPattern/MediatorPatternSolution/WithMediatorPattern.java
@@ -1,45 +1,59 @@
+
 package org.onkar.DesignPattern.MediatorPatternSolution;
 
 import java.util.ArrayList;
 import java.util.List;
 
-interface ChatMediator{
-	void sendMessages();
+// Interface chatMediator
+interface ChatMediator {
+	void sendMessages(String msg, User user);
+
 	void addUser(User user);
 }
 
-class ChatRoom implements ChatMediator{ 
-	
+// class chatRoom implements chatMeadiator.
+class ChatRoom implements ChatMediator {
+
 	private List<User> users;
-	
-	void ChatRoom() {
-		this.users = new ArrayList<>(); 
+
+	public ChatRoom() {
+		this.users = new ArrayList<>();
 	}
 
 	@Override
-	public void sendMessages() {
-		
-		
+	public void sendMessages(String msg, User sender) {
+		for (User user : users) {
+			if (user != sender) {
+				user.receiveMessage(msg, sender);
+			}
+		}
 	}
 
 	@Override
 	public void addUser(User user) {
-	
-		users.add(user); 	
+
+		users.add(user);
 	}
-	
 }
 
+// chat user class
 class User {
 
 	private String name;
+	private ChatMediator chatRoom;
 
-	public User(String string) {
+	public User(String string, ChatMediator chatRoom2) {
 		this.name = string;
+		this.chatRoom = chatRoom2;
 	}
 
-	public void sendMessage(String msg, User receipient) {
-		System.out.println(this.name + "Sending message " + msg);
+	public void sendMessage(String msg) {
+		System.out.println(this.name + " Sending message: " + msg);
+		chatRoom.sendMessages(msg, this);
+	}
+
+	public void receiveMessage(String msg, User sender) {
+		System.out.println(this.name + " receive message: " + msg + "from " + sender.getaName());
 	}
 
 	public String getaName() {
@@ -50,8 +64,16 @@ class User {
 public class WithMediatorPattern {
 
 	public static void main(String[] args) {
-		 
 
+		ChatMediator chatRoom = new ChatRoom();
+		User rohit = new User("Rohit", chatRoom);
+		User subha = new User("Subha", chatRoom);
+		User sachin = new User("Sachin", chatRoom);
+
+		chatRoom.addUser(sachin);
+		chatRoom.addUser(subha);
+		chatRoom.addUser(rohit);
+
+		rohit.sendMessage("Hello all how are you!!");
 	}
-
 }
